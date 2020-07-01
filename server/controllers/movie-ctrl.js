076e131988df928c79dc5e1030c92257ac17c3a0
ankan <ankanb49@gmail.com>
2020-07-02 01:27:53 +0530

    server done

--- server/controllers/movie-ctrl.js
+++ server/controllers/movie-ctrl.js
@@ -1,8 +1,8 @@
 var Movie = require("../models/movie-model");
 
-//creates a new movie
+//adding movies here
 createMovie = (req, res) => {
-  const body = req.body;
+  var body = req.body;
 
   if (!body) {
     return res.status(400).json({
@@ -10,13 +10,15 @@ createMovie = (req, res) => {
       error: "You must provide a movie",
     });
   }
-  //need to check later if changing Movie to movies still works or not
-  const movie = new Movie(body);
+
+  var movie = new Movie(body);
+
   if (!movie) {
     return res.status(400).json({ success: false, error: err });
   }
 
-  movie.save
+  movie
+    .save()
     .then(() => {
       return res.status(201).json({
         success: true,
@@ -32,4 +34,91 @@ createMovie = (req, res) => {
     });
 };
 
-//updates the data of a movie
+//updating movies here
+updateMovie = async (req, res) => {
+  var body = req.body;
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: "You must provide a body to update",
+    });
+  }
+
+  Movie.findOne({ _id: req.params.id }, (err, movie) => {
+    if (err) {
+      return res.status(404).json({
+        err,
+        message: "Movie not found!",
+      });
+    }
+    movie.name = body.name;
+    movie.time = body.time;
+    movie.rating = body.rating;
+    movie
+      .save()
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          id: movie._id,
+          message: "Movie updated!",
+        });
+      })
+      .catch((error) => {
+        return res.status(404).json({
+          error,
+          message: "Movie not updated!",
+        });
+      });
+  });
+};
+
+//deleting movies here
+deleteMovie = async (req, res) => {
+  await Movie.findOneAndDelete({ _id: req.params.id }, (err, movie) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
+
+    if (!movie) {
+      return res.status(404).json({ success: false, error: `Movie not found` });
+    }
+
+    return res.status(200).json({ success: true, data: movie });
+  }).catch((err) => console.log(err));
+};
+
+//getting movies by id here
+getMovieById = async (req, res) => {
+  await Movie.findOne({ _id: req.params.id }, (err, movie) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
+
+    if (!movie) {
+      return res.status(404).json({ success: false, error: `Movie not found` });
+    }
+    return res.status(200).json({ success: true, data: movie });
+  }).catch((err) => console.log(err));
+};
+
+//getting all movies
+getMovies = async (req, res) => {
+  await Movie.find({}, (err, movies) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err });
+    }
+    if (!movies.length) {
+      return res.status(404).json({ success: false, error: `Movie not found` });
+    }
+    return res.status(200).json({ success: true, data: movies });
+  }).catch((err) => console.log(err));
+};
+
+module.exports = {
+  createMovie,
+  updateMovie,
+  deleteMovie,
+  getMovies,
+  getMovieById,
+};
--- server/index.js
+++ server/index.js
@@ -1,20 +1,23 @@
 var express = require("express");
-var app = express();
 var bodyParser = require("body-parser");
 var cors = require("cors");
+
 var db = require("./db");
+var movieRouter = require("./routes/movie-router");
+
+var app = express();
 var apiPort = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use(bodyParser.json());
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Hello World!");
 });
 
-app.listen(apiPort, () => {
-  console.log(`Server running on port ${apiPort}`);
-});
+app.use("/api", movieRouter);
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
